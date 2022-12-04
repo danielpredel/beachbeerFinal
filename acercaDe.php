19fd3e1f1e44a84e61069e8554c6ae2bd80bd63b
carrplas <juliancarrplas@gmail.com>
2022-12-04 04:19:46 -0600

    Agregué cambios a Acercade, al index y puse mas imágenes

---
+++ acercaDe.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/estilosAcercaDe.css">
+</head>
+<?php
+    include "head.php";
+?>
+<body>
+    <div class="Contenedor22">
+        <div class="contenedor1J">
+            <img class="image11" src="img/cheve.png" alt=""></div>
+        <div class="contenedor2J">
+            <p class="text1J">Acerca de</p> <br>
+            <br>
+            <br>
+            <br>
+            <p class="text2J">Beach</p> <br><br><br>
+            <p class="text2J">Beer</p> <br>
+            <br>
+            <br>
+            <br>
+            <p class="text3J">Beach Beer es la empresa líder en México en la elaboración, distribución y venta de cervezas y licores. Mantenemos un firme compromiso con el fomento de un mundo mejor a través del consumo responsable, iniciativas ambientales y apoyo a las comunidades en que vivimos y trabajamos. Nos distingue como empresa, cumplir nuestras responsabilidades con los consumidores, clientes, accionistas y empleados a través de la dirección, liderazgo e innovación.</p>
+        </div>
+    </div>
+    <div>
+        <p class="titulo2">Nuestro equipo</p>
+    </div>
+    <div class="imagenes">
+        <div class="imagenes2">
+            <div class="image1">
+            <img class="imagg" src="img/Kevin.jpg" alt="">
+            <p class="tit">Kevin de Jesus</p>
+            <p class="titu">La mente del equipo</p>
+            </div>
+            <div class="image2">
+            <img class="imagg" src="img/Daniel.jpg" alt="">
+            <p class="tit">Daniel Preciado</p>
+            <p class="titu">El que ve memes en clases</p>
+            </div>
+        </div>
+        <br>
+        <div class="imagenes3">
+            <div class="image1">
+            <img class="imagg" src="img/Alan.jpg" alt="">
+            <p class="tit">Alan Uziel</p>
+            <p class="titu">El todas mías</p>
+            </div>
+            <div class="image2">
+            <img class="imagg" src="img/Julian_edited.jpg" alt="">
+            <p class="tit">Julián Carreón</p>
+            <p class="titu">El mandilón</p>
+            </div>
+        </div>
+        <br>
+        <div class="imagenes4">
+            <div class="image1">
+            <img class="imagg" src="img/diaz.jpeg" alt="">>
+            <p class="tit">Zaid Díaz</p>
+            <p class="titu">El que no hace nada</p>
+            </div>
+            <div class="image2">
+            <img class="imagg" src="img/omar.jpeg" alt="">
+            <p class="tit">Omar Alonso</p>
+            <p class="titu">El despechá</p>
+            </div>
+        </div>
+        <br>
+    </div>
+    <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3702.3769632783897!2d-102.29735748573928!3d21.88155516349525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8429ee7ca61d5813%3A0xd5c973c65f839335!2sAv.%20Francisco%20I.%20Madero%20123%2C%20Zona%20Centro%2C%2020000%20Aguascalientes%2C%20Ags.!5e0!3m2!1ses-419!2smx!4v1670001749694!5m2!1ses-419!2smx" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    <?php
+    include "footer.php";
+    ?>    
+</body>
+</html>
